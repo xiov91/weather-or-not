f3b59cf39c670c87602b2d6d09c022eb63c5099d
Zee91 <xiovacc@gmail.com>
2022-06-09 11:10:30 -0400

    pulls coordinates properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,45 @@ var inputValue = document.querySelector('.inputValue')
 var name = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
+var lat = document.querySelector('.lat');
+var lon = document.querySelector('.lon');
 
-button.addEventListener('click',function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=2dde1f911405b1e8128db341683f302b')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .then(data => {
+function getCurrent(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=imperial&appid=2dde1f911405b1e8128db341683f302b')       
+        .then(response => response.json())
+        .then(data => {
         var nameValue = data['name'];
         var tempValue = data['main']['temp'];
         var descValue = data['weather'][0]['description'];
-        var latValue = data['lat'];
-        var lonValue = data['lon'];
+        var lonValue = data['coord']['lon'];
+        var latValue = data['coord']['lat'];
 
         name.innerHTML = nameValue;
         temp.innerHTML = tempValue;
-        desc.innerHTML = descValue; 
+        desc.innerHTML = descValue;
     })
-});
+}
+
+button.addEventListener('click', function () {
+    getCurrent();
+    fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + inputValue.value + '&limit=1&appid=2dde1f911405b1e8128db341683f302b')
+        .then(response => response.json())
+        .then(data => {
+            var latValue = data[0]['lat'];
+            var lonValue = data[0]['lon'];
+
+            console.log(latValue, lonValue)
+        })
+    });
+
+    /* fetch('https://api.openweathermap.org/data/3.0/onecall?lat=' + lanValue.value + '&lon=' + lonValue.value + '&exclude=minutely,hourly,alerts&appid=2dde1f911405b1e8128db341683f302b')
+        .then(response => response.json())
+        .then(data => {
+            var nameValue = data['name'];
+            var tempValue = data['main']['temp'];
+            var descValue = data['weather'][0]['description'];
+
+            name.innerHTML = nameValue;
+            temp.innerHTML = tempValue;
+            desc.innerHTML = descValue; */
+        
